425377269031f7b548dd57936f18af63c9326395
Tony Alvarez <tonyalva87@gmail.com>
2022-07-12 15:06:57 -0700

    add api call

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import PlayingCard from './PlayingCard'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <div className="App">
--- src/PlayingCard.jsx
+++ src/PlayingCard.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import Card from '@mui/material/Card';
+import Box from '@mui/material/Box';
 
 // Cards API
 const BASE_API = "https://deckofcardsapi.com/api/deck/";
@@ -8,24 +9,21 @@ const SHUFFLE_API = "new/shuffle/?deck_count=1"
 
 // Returns deck id
 const getNewDeck = async () => {
-  const response = await axios.get(BASE_API + SHUFFLE_API)
-  console.log(response)
-  return response
+  const resp = await axios.get(BASE_API + SHUFFLE_API)
+  console.log(resp.data)
+  return resp.data.deck_id
 }
 
-const drawCardFromDeck = async (deck_id, count = 1) => {
+const drawCardFromDeck = async (count=1) => {
+  let deck_id = await getNewDeck()
   let response = await axios.get(BASE_API + `${deck_id}/draw/?count=${count}`)
   console.log(response)
 }
 
-const drawCard = `${deck_id}/draw/?count=2`
-
-
 const PlayingCard = () => {
   return (
     <Box>
-      <img src="test" alt="Test"/>
-      getNewDeck()
+      {drawCardFromDeck()}
     </Box>
   )
 }
