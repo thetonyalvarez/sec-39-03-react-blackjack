dde1206e810df2f8aae0722f90799635275d7d3a
Tony Alvarez <tonyalva87@gmail.com>
2022-07-12 17:17:18 -0700

    complete blackjack components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
-import logo from './logo.svg'
-import PlayingCard from './PlayingCard'
+import logo from './blackjack.png'
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import PlayingHand from './PlayingHand'
 import './App.css'
 
 function App() {
@@ -9,9 +11,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Hello Vite + React!</p>
-        <PlayingCard/>
+        <img className="App-logo" src={logo} alt="Blackjack!" />
       </header>
+      <main className="App-main">
+        <Button color="secondary" variant="contained" href="/">
+          Deal New Hand
+        </Button>
+        <PlayingHand/>
+      </main>
     </div>
   )
 }
--- src/PlayingCard.jsx
+++ src/PlayingCard.jsx
@@ -3,26 +3,9 @@ import axios from 'axios';
 import Card from '@mui/material/Card';
 import Box from '@mui/material/Box';
 
-// Cards API
-const BASE_API = "https://deckofcardsapi.com/api/deck/";
-const SHUFFLE_API = "new/shuffle/?deck_count=1"
-
-// Returns deck id
-const getNewDeck = async () => {
-  const resp = await axios.get(BASE_API + SHUFFLE_API)
-  return resp.data.deck_id
-}
-
-const drawCardFromDeck = async (count=1) => {
-  let deck_id = await getNewDeck()
-  let response = await axios.get(BASE_API + `${deck_id}/draw/?count=${count}`)
-}
-
-const PlayingCard = () => {
+const PlayingCard = (props) => {
   return (
-    <Box>
-      {drawCardFromDeck()}
-    </Box>
+    <img className="PlayingCard-image" src={props.image}/>
   )
 }
 
